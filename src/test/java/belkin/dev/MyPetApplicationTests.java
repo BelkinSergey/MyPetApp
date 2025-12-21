a305529fba7e7bd66dba2c85d17445dccb2f2d45
Sergey Belkin <belkinserega95@yandex.ru>
2025-12-21 16:56:50 +0300

    first commit

---
+++ src/test/java/belkin/dev/MyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package belkin.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
